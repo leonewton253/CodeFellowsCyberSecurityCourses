afaa012a1b2a331445b6303c0fd9587368d68a59
Leo Newton <leo.newton@icloud.com>
2023-09-21 19:31:04 -0700

    first

---
+++ helloworld.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Script Name:                  HelloWorld
+# Author:                       Leo Newton
+# Date of latest revision:      09/21/2023
+# Purpose:                      Print "Hello World"
+
+# Declaration of variables
+
+# Declaration of functions
+
+
+# Main
+echo "hello world"
+
+
+# End
